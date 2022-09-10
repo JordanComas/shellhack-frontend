e36cb30e51ea84dc7b37362766719a335121e2d9
JordanComas <jordancomas1@yahoo.com>
2022-09-10 01:03:39 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Signup from "./components/Signup";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route to="/" element={<Home />} />
+        <Route to="/login" element={<Login />} />
+        <Route to="/signup" element={<Signup />} />
+        <Route to="/profile" element={<Profile />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../components/navbar.css";
+
+const Navbar = () => {
+  return <nav></nav>;
+};
+
+export default Navbar;
