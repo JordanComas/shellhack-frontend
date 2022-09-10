726ce05778383bd7cb40f7d5b12db242380fc1c4
JordanComas <jordancomas1@yahoo.com>
2022-09-10 16:13:32 -0400

    more dashboard edits

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,21 +5,14 @@ import { Link } from "react-router-dom";
 import DonutChart from "react-donut-chart";
 import { useNavigate } from "react-router-dom";
 
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
 const Dashboard = () => {
-  // const [totalExpenses, setTotalExpenses] = React.useState(0);
-  // const [listExpenses, setListExpenses] = React.useState([
-  //   { value: 20 },
-  //   { value: 45 },
-  //   { value: 50 },
-  // ]);
+  const [percentage, setPercentage] = React.useState(70);
+  // const percentage = 10;
 
-  // setTotalExpenses([{}]);
   const navigate = useNavigate();
-
-  const chartLink = () => {
-    navigate("/");
-    console.log("clicked");
-  };
 
   return (
     <div className="dashboard">
@@ -40,26 +33,76 @@ const Dashboard = () => {
           <h4>Subscriptions</h4>
         </Link>
       </div>
-
-      {/* <ul>
-        {listExpenses
-          .sort((a, b) => {
-            return b.value - a.value;
-          })
-          .map((expense) => {
-            return <li>{expense.amount}</li>;
-          })}
-      </ul> */}
       <div className="sections">
         <div className="chart-left">
           <h1>Welcome to your dashboard Jordan!</h1>
-          <h1>ADD GOAL BAR Here</h1>
+          <div className="progress">
+            <div className="progress-section">
+              <h3>Saving for a car</h3>
+              <CircularProgressbar
+                value={percentage}
+                text={`${percentage}%`}
+                styles={{
+                  path: {
+                    // Path color
+                    stroke: `rgba(62, 152, 199, ${percentage / 100})`,
+                  },
+                  text: {
+                    // Text color
+                    fill: "#2d6590",
+                    // Text size
+                    fontSize: "16px",
+                  },
+                }}
+              />
+            </div>
+            <div className="progress-section">
+              <h3>Saving for a car</h3>
+              <CircularProgressbar
+                value={percentage}
+                text={`${percentage}%`}
+                styles={{
+                  path: {
+                    // Path color
+                    stroke: `rgba(62, 152, 199, ${percentage / 100})`,
+                  },
+                  text: {
+                    // Text color
+                    fill: "#2d6590",
+                    // Text size
+                    fontSize: "16px",
+                  },
+                }}
+              />
+            </div>
+            <div className="progress-section">
+              <h3>Saving for a car</h3>
+              <CircularProgressbar
+                value={percentage}
+                text={`${percentage}%`}
+                styles={{
+                  path: {
+                    // Path color
+                    stroke: `rgba(62, 152, 199, ${percentage / 100})`,
+                  },
+                  text: {
+                    // Text color
+                    fill: "#2d6590",
+                    // Text size
+                    fontSize: "16px",
+                  },
+                }}
+              />
+            </div>
+          </div>
         </div>
         <div className="chart">
           <DonutChart
             // className="donutchart"
             // colors={["#2d6590", "#daeaf6"]}
             // onClick={(item, toggled) => (toggled ? item : navigate("/"))}
+            // width={800}
+            // height={100}
             clickToggle={false}
             data={[
               {
@@ -71,17 +114,34 @@ const Dashboard = () => {
                 value: 300,
               },
               {
-                label: "Savings",
+                label: "Accesories",
                 value: 1000,
               },
               {
                 label: "Empty",
-                value: 10,
+                value: 90,
                 isEmpty: true,
               },
             ]}
           />
-          ;
+          <DonutChart
+            // className="donutchart"
+            colors={["red", "green"]}
+            // onClick={(item, toggled) => (toggled ? item : navigate("/"))}
+            // width={800}
+            // height={100}
+            clickToggle={false}
+            data={[
+              {
+                label: "Income",
+                value: 500,
+              },
+              {
+                label: "Outcome",
+                value: 300,
+              },
+            ]}
+          />
         </div>
       </div>
     </div>
